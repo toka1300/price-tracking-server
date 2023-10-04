73ecfb2a1d2833428dbfd8bbdca55a92b040d047
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-10-04 02:33:48 -0400

    fetch can retrieve multiple ids

--- server.mjs
+++ server.mjs
@@ -10,31 +10,33 @@ app.use((req, res, next) => {
 })
 
 const parseEventInfo = (data) => {
-  console.log(data);
   const jsonMatch = data.match(/<script id="index-data" type="application\/json">\s*(.*?)\s*<\/script>/);
   if (!jsonMatch || jsonMatch.length < 2) return;
   const jsonData = jsonMatch[1];
   const parsedJson = JSON.parse(jsonData);
-  console.log(parsedJson);
   const eventObject = {
     name: parsedJson.eventName,
     url: parsedJson.eventUrl,
     venue: parsedJson.venueName,
     date: parsedJson.formattedEventDateTime,
-    minPrice: parsedJson.grid.minPrice
+    minPrice: Math.round(parsedJson.grid.minPrice)
   };
   return eventObject
 }
 
 app.get('/get-event-info', async (req, res) => {
-  const eventIds = req.query.id;
-  const url = `https://www.stubhub.ca/event/${eventIds}/?quantity=1`
+  const results = [];
+  const eventIds = req.query.id.split(',');
 
   try {
-    const response = await fetch(url);
-    const data = await response.text();
-    const priceObject = parseEventInfo(data);
-    res.send(priceObject);
+    await Promise.all(eventIds.map(async (id) => {
+      const url = `https://www.stubhub.ca/event/${id}/?quantity=1`;
+      const response = await fetch(url);
+      const data = await response.text();
+      const priceObject = parseEventInfo(data);
+      results.push(priceObject);
+    }))
+    res.send(results);
   } catch (error) {
     res.status(500).send('Error fetching data')
   }
